669ce1929b24a33270d6d954dc9db157dd871ce9
Ivan Mikhalev <gorilladev@MacBook-Air-Ivan.local>
2023-09-07 10:35:12 +0500

    Add react-roter and lazy load

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import Counter from "../Counter/Counter";
 import './index.scss';
+import {Link, Route, Routes,} from "react-router-dom";
+import {MainPageAsync} from "../../pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "../../pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
-        <>
-            <div className='content'>
-                324234324
-            </div>
+        <div className='app'>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/About'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
             <Counter/>
-        </>
+        </div>
     );
 };
 
