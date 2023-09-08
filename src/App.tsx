845a81546c5dfc0a3dc3fc365504c0908e82b5be
Ivan Mikhalev <gorilladev@MacBook-Air-Ivan.local>
2023-09-08 14:21:33 +0400

    Fetch: className by function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React, {Suspense, useContext, useState} from 'react';
-import './styles/index.scss';
+import {Suspense} from 'react';
 import {Link, Route, Routes,} from "react-router-dom";
+
+import './styles/index.scss';
+
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+
 import useTheme from "./theme/useTheme";
+
+import {classNames} from "./helpers/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <Link to={'/'}>Main</Link>
             <Link to={'/About'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
---
+++ src/helpers/classNames.ts
@@ -0,0 +1,14 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(className: string, mods: Mods, additional: string[]): string {
+    const resultModes = Object.entries(mods)
+        .filter(([className, boolean]) => boolean === true)
+        .map(([className, boolean]) => className)
+        .join(' ');
+
+    return [
+        className,
+        ...additional,
+        resultModes
+    ].join(' ')
+}
