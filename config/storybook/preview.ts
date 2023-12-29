6d9d91cad4ca8fb778d45c35774d2c635c10d4f1
Gorilla Dev <gorilladevq@gmail.com>
2023-12-29 12:21:09 +0300

    Update Storybook v7

---
+++ config/storybook/preview.ts
@@ -0,0 +1,25 @@
+import type { Preview } from '@storybook/react';
+import { StyleDecorator } from 'shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+};
+
+export const decorators = [
+    StyleDecorator(),
+    ThemeDecorator(Theme.LIGHT),
+    RouterDecorator(),
+];
+
+export default preview;
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { buildCssLoader } from '../builds/loaders/buildCssLoader';
 import { BuildPaths } from '../builds/types/config';
 import { buildSvgLoader } from '../builds/loaders/buildSvgLoader';
+import { buildBabelLoader } from '../builds/loaders/BuildBabelLoader';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
@@ -14,15 +15,16 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
     config.module.rules.push(buildCssLoader(true));
+    config.module.rules.push(buildBabelLoader(false, true));
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
-            return { ...rule, exclude: /\.svg/i };
-        }
-
-        return rule;
-    });
-    config.module.rules.push(buildSvgLoader());
+    // config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    //     if (/svg/.test(rule.test as string)) {
+    //         return { ...rule, exclude: /\.svg/i };
+    //     }
+    //
+    //     return rule;
+    // });
+    // config.module.rules.push(buildSvgLoader());
 
     return config;
 };
